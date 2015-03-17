c7206424f0485d69fc38845850fc88145a8c0cd3
prisonblues <github@prisonblues.fastmail.fm>
2015-03-17 16:13:32 +0000

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bThe Cloud\b/g, "Other People's Computers");
+	v = v.replace(/\bThe cloud\b/g, "Other people's computers");
+	v = v.replace(/\bthe Cloud\b/g, "other people's computers");
+	v = v.replace(/\bthe cloud\b/g, "other people's computers");
 	
 	textNode.nodeValue = v;
 }
